c31c47ea8d56ea2bd708ff9753627dd074eb137d
chalatu <1841220662@qq.com>
2025-11-30 00:55:14 +0800

    "2052"

---
+++ cpp_code/2052.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, m;
+	while (cin >> n >> m) {
+	cout << "+";
+	for (int i = 0; i < n; ++i) {
+		cout << "-";
+	}
+	cout << "+" << endl;
+	for (int i = 0; i < m; ++i) {
+		cout << "|";
+		for (int j = 0; j < n; ++j) {
+			cout << " ";
+		}
+		cout << "|" << endl;
+	}
+	cout << "+";
+	for (int i = 0; i < n; ++i) {
+		cout << "-";
+	}
+	cout << "+" << endl;
+	}
+}
