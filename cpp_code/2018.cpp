3b7d89ace62ed34332bf106add2d8d87aefd7c4b
chalatu <1841220662@qq.com>
2026-07-17 20:07:07 +0800

    "2018"

--- cpp_code/2018.cpp
+++ cpp_code/2018.cpp
@@ -2,16 +2,29 @@
 
 using namespace std;
 
+long long int fun(long long int n,long long int res){
+    if(0<n && n<=3){
+        res = n;
+        return res;
+    }
+    else if(n >= 4){
+        res = fun(n - 1,res) +fun(n - 3,res);
+        return res;
+    }
+    
+}
+
 int main(){
-    int n;
-    int res;
+    long long int n=0;
+    long long int res=0;
     while(cin>>n){
         if (n==0)break;
-        else if ( 0 < n && n<=4){
+        else if ( 0 < n && n < 4){
             cout<<n<<endl;
+            
         }
-        else if (n>4){
-            res = (n % 4) + (n / 4);
+        else if (n >= 4){
+            res = fun(n,res);
             cout<<res<<endl;
         } 
     }
